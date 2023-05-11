627afc05f1b616d1e96ee6d9424deb39dd8e2c19
Suhaib Chowdhury <suchowdhury@ucsd.edu>
2023-05-11 15:06:19 -0700

    Commit 1

--- TestListExamples.java
+++ TestListExamples.java
@@ -3,6 +3,8 @@ import org.junit.*;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.xml.crypto.dsig.spec.XPathType.Filter;
+
 class IsMoon implements StringChecker {
   public boolean checkString(String s) {
     return s.equalsIgnoreCase("moon");
@@ -10,6 +12,14 @@ class IsMoon implements StringChecker {
 }
 
 public class TestListExamples {
+  @Test(timeout = 500)
+  public void testFilter(){
+    StringChecker checker = new IsMoon();
+    List<String> input = Arrays.asList("moon", "mon", "MOON", "mOOn", "mmoon");
+    List<String> expected = Arrays.asList("moon", "MOON", "mOOn");
+    assertEquals("Should report strings equal to \'moon\' ignoring case.", expected, ListExamples.filter(input, checker));
+  }
+
   @Test(timeout = 500)
   public void testMergeRightEnd() {
     List<String> left = Arrays.asList("a", "b", "c");
--- grade.sh
+++ grade.sh
@@ -8,6 +8,33 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+if [[ -f student-submission/ListExamples.java ]]
+then
+    echo found student submission
+else
+    echo ListExamples.java not found
+    exit 1
+fi
+
+cp student-submission/*.java grading-area
+cp TestListExamples.java grading-area
+
+echo moved files
+
+javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" grading-area/*.java
+# javac grading-area/*.java
+
+if [[ $? != 0 ]]
+then 
+    echo Compilation Failed
+    exit 1
+fi
+
+echo compiled files
+
+ls grading-area
+
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar;grading-area" org.junit.runner.JUnitCore TestListExamples > output.txt
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
