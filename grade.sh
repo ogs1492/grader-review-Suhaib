705f390ad029ad7ff5343419469b1a8164d75cd9
ogs1492 <36433418+ogs1492@users.noreply.github.com>
2023-05-25 15:10:37 -0700

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -19,7 +19,7 @@ fi
 cp student-submission/*.java grading-area
 cp TestListExamples.java grading-area
 
-javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar grading-area/*.java 2> error.txt
+javac -cp $CPATH grading-area/*.java 2> error.txt
 # javac grading-area/*.java
 
 if [[ $? != 0 ]]
@@ -30,7 +30,7 @@ then
 fi
 
 
-java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar;grading-area" org.junit.runner.JUnitCore TestListExamples > output.txt
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > output.txt
 
 result=`grep "OK (2 tests)" output.txt`
 grep_result="OK (2 tests)"
